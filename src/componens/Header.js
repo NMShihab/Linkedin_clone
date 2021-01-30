c6edfad883e6a2ff4e6f9bb4e10fe4aecf5b64f0
NMShihab <nmshihabislam@gmail.com>
2021-01-30 21:36:22 +0600

    header part

--- src/componens/Header.js
+++ src/componens/Header.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import "../css/Header.css"
 import SearchIcon from '@material-ui/icons/Search';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import PersonIcon from '@material-ui/icons/Person';
+import ChatIcon from '@material-ui/icons/Chat'
+
+import HeaderOption from './HeaderOption';
 
 function Header() {
     return (
@@ -21,6 +28,16 @@ function Header() {
             </div>
 
             <div className="header__right">
+                <HeaderOption Icon = {HomeIcon} title = 'Home'/>
+                <HeaderOption Icon ={SupervisorAccountIcon} title = 'My network'/>
+                <HeaderOption Icon = {BusinessCenterIcon} title = 'Jobs'/>
+                <HeaderOption Icon ={ChatIcon} title = 'Messaging'/>
+                <HeaderOption Icon ={NotificationsIcon} title = 'Notifications'/>
+                <HeaderOption avatar ={PersonIcon} title = 'me'/>
+                
+
+                
+
 
             </div>
             
