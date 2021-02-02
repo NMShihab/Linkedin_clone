7b544bf8455da93693c7ea8406182e44cf775fc2
NMShihab <nmshihabislam@gmail.com>
2021-02-02 17:41:30 +0600

    signup styling

--- src/componens/Login.js
+++ src/componens/Login.js
@@ -2,9 +2,30 @@ import React from 'react'
 import '../css/Login.css'
 
 function Login() {
+    const logIn = () =>{
+
+    }
+    const signUp = () =>{
+
+    }
     return (
         <div className='login'>
-            <h1>You are not logged in</h1>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/8/80/LinkedIn_Logo_2013.svg" alt=""/>
+
+            <div className="login__card">
+                <form>
+                    <input type="text" placeholder = "Full name"/>
+                    <input type="email" placeholder = "Email"/>
+                    <input type="password" placeholder = "Password"/>
+
+                    <button type = 'submit' onClick = {signUp}>Agree & join</button>
+                </form>
+
+                <p>Already on Linkedin? <span className = 'login__join' onClick = {logIn}>
+                                        Sign in</span></p>
+
+            </div>
+            
             
         </div>
     )
